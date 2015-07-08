eb5082425181784329990fce8f07ae45347bb99e
ywu66 <jokerjoe@Macs-MacBook-Pro.local>
2015-07-08 17:50:56 -0400

    Create layout & styling for main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,7 @@ angular.module('cm2App')
   .controller('rootCtrl', function ($scope, getUserService) {
     getUserService.getUser().$promise.then(function(response) {
       $scope.userName = response.userName;
+      $scope.date = new Date();
     });
 /*response: json response from server*/
     console.log('Root state');
