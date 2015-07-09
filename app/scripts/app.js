2094f8e86fa71e770a8486baf2b6418ce5fc2b42
ywu66 <jokerjoe@Macs-MacBook-Pro.local>
2015-07-09 17:24:46 -0400

    Modified some color and wireframe for main page. Merged button function into viewContractCtrl.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,27 +9,32 @@
  * Main module of the application.
  */
 angular
-  .module('cm2App', [
-    'ngAnimate',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ui.router',
-    'ui.bootstrap'
-  ])
-  .config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/root/viewContract');
+    .module('cm2App', [
+        'ngAnimate',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ui.router',
+        'ui.bootstrap'
+    ])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/root/viewContract');
 
-    $stateProvider
-      .state('root', {
-        url: '/root',
-        templateUrl: 'views/main.html',
-        controller: 'rootCtrl',
-        abstract: true
-      })
-      .state('root.viewContract', {
-        url: '/viewContract',
-        templateUrl: 'views/viewContract.html',
-        controller: 'viewContractCtrl'
-      });
-  });
+        $stateProvider
+            .state('root', {
+                url: '/root',
+                templateUrl: 'views/main.html',
+                controller: 'rootCtrl',
+                abstract: true
+            })
+            .state('root.viewContract', {
+                url: '/viewContract',
+                templateUrl: 'views/viewContract.html',
+                controller: 'viewContractCtrl'
+            })
+            .state('root.manageContract', {
+                url: '/manageContract',
+                templateUrl: 'views/manageContract.html',
+                controller: 'manageContractCtrl'
+            });
+    });
